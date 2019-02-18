33b9be2525331d774f6c708e587d845358bdf51f
MarekMaz <emarcus255@gmail.com>
2019-02-18 16:42:23 +0000

    Added some extra links and information about MT-Hub (Funded by INEA, GIT link, Licence link)

--- Layout/footer.php
+++ Layout/footer.php
@@ -1,6 +1,9 @@
 <footer class="main-footer">
   <span>
       Copyright &copy; <?php echo date("Y"); ?> |
-      <a href="https://mthub.docs.apiary.io/#" target="_blank"><?php echo Session::t('Api Documentation'); ?></a>
+      Published under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPL 3.0</a> |
+      Downloadable from <a href="https://github.com/iADAATPA/mt-hub" target="_blank">iADAATPA</a> |
+      <a href="https://mthub.docs.apiary.io/#" target="_blank">Api Documentation</a> |
+      Funded by INEA - <a href="https://ec.europa.eu/inea/en/connecting-europe-facility/cef-telecom/2016-eu-ia-0132" target="_blank">Grant # 2016-EU-IA-0132</a>
   </span>
 </footer>
